be7b22de6584e5aeb0356ebf7860f8cb0e37f504
MathiasJohansen <mathiaslj2011@hotmail.no>
2023-02-03 09:49:52 +0100

    er

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import './css/main.css'
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Det funker
+      
+     </h1>
+    </div>
+  );
+}
+
+export default App;
